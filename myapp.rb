97f21b863d1cf32be2365b77e712412387691ec2
USER <user@MacBook-Air.local>
2017-09-26 11:09:23 +0900

    aa naoshi2

--- myapp.rb
+++ myapp.rb
@@ -111,7 +111,7 @@ end
 
 post '/aa_trans' do
 	@na = params[:na]
-	@trans_na = translate(params[:na]).gsub("\n","")
+	@trans_na = translate(params[:na]).gsub("\n","").gsub(" ","")
 	@bunshiryou = bunshi_calc(@trans_na)
 	erb :aa_trans
 end
